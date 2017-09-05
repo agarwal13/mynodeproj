c19f9987ad8ed671ea25ee7ef8c627d1a5cffc46
agarwal13 <narendra.agarwal@healthasyst.com>
2017-09-05 05:46:05 +0530

    committing some uncommitted changes

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const http = require('http');
-//const config = require('./config/config.json');
-import environment from './config/env.js';
-const config = require(process.argv[2]).get('production');
-console.log(config.env);
+//const config = require('./config/env.js');
+//import environment from './config/env.js';
+//const config = require(process.argv[2]).get('production');
+const config = require('./config/'+process.env.NODE_ENV+'.json');
+console.log("Environment is " + config.env);
+//console.log("Auth IP Address is " + process.env.AUTH_SERVICE);
 
-console.log(environment.get('development'));
+//console.log(environment.get('development'));
 
 var app = express();
-app.set('port', 4444);
 var metadata = {
     "author": "Narendra",
     "language": "node"
@@ -18,7 +19,7 @@ console.log(process.env.NODE_ENV);
 app.set('meta', metadata);
 const server = http.createServer(app);
 
-server.listen(app.get('port'), function() {
-    console.log(`Node is running at localhost:${app.get('port')}`);
+server.listen(config.port, function() {
+    console.log(`Node is running at localhost:${config.port}`);
     console.log(app.get('meta').author);
 });
